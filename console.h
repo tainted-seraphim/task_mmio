ab5ab03468a8e7e3bcc851c62af2baef015f77a1
tainted-seraphim <savosin_ya@mail.ru>
2025-03-04 01:37:21 +0300

    console command wrappers

--- console.h
+++ console.h
@@ -27,5 +27,44 @@ void console_do_action_for_char(struct console *con, char ch);
 
 void console_execute_command(struct console *con);
 
+typedef int (*console_command_handler)(struct console *con, size_t, char **);
+
+struct console_command_wrapper {
+	const char *command;
+	console_command_handler handler;
+};
+
+int console_command_help(struct console *con, size_t argc, char **argv);
+
+int console_command_quit(struct console *con, size_t argc, char **argv);
+
+int console_command_clear(struct console *con, size_t argc, char **argv);
+
+int console_command_history(struct console *con, size_t argc, char **argv);
+
+int console_command_iorb(struct console *con, size_t argc, char **argv);
+
+int console_command_iorw(struct console *con, size_t argc, char **argv);
+
+int console_command_iord(struct console *con, size_t argc, char **argv);
+
+int console_command_iowb(struct console *con, size_t argc, char **argv);
+
+int console_command_ioww(struct console *con, size_t argc, char **argv);
+
+int console_command_iowd(struct console *con, size_t argc, char **argv);
+
+int console_command_mmrb(struct console *con, size_t argc, char **argv);
+
+int console_command_mmrw(struct console *con, size_t argc, char **argv);
+
+int console_command_mmrd(struct console *con, size_t argc, char **argv);
+
+int console_command_mmwb(struct console *con, size_t argc, char **argv);
+
+int console_command_mmww(struct console *con, size_t argc, char **argv);
+
+int console_command_mmwd(struct console *con, size_t argc, char **argv);
+
 #endif
 
