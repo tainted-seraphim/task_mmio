e0bf2e05b59829f6e4717c17c4889ffae5660a99
tainted-seraphim <savosin_ya@mail.ru>
2025-03-03 23:22:41 +0300

    removed get_esc_type and arrow key 'symbols'

--- chars.c
+++ chars.c
@@ -19,33 +19,12 @@ enum char_type chars_get_char_type(char ch)
 	case 127:
 		return CHAR_BACKSPACE;
 	case 27:
-		return chars_get_esc_sequence();
+		return CHAR_ESC;
 	default:
 		if (ch >= 32 && ch <= 126) {
 			return CHAR_PRINTABLE;
 		}
 		return CHAR_UNSUPPORTED;
 	}
-}
-
-enum char_type chars_get_esc_sequence(void)
-{
-	char buff[8] = {27};
-	read(1, buff + 1, 7);
-	buff[7] = '\0';
-	/* This is ugly but I have to do this for input */
-	if (0 == strcmp(buff, TASK_MMIO_ESC_CURSOR_UP)) {
-		return CHAR_ARROW_UP;
-	}
-	if (0 == strcmp(buff, TASK_MMIO_ESC_CURSOR_DOWN)) {
-		return CHAR_ARROW_DOWN;
-	}
-	if (0 == strcmp(buff, TASK_MMIO_ESC_CURSOR_RIGHT)) {
-		return CHAR_ARROW_RIGHT;
-	}
-	if (0 == strcmp(buff, TASK_MMIO_ESC_CURSOR_LEFT)) {
-		return CHAR_ARROW_LEFT;
-	}
-	return CHAR_UNSUPPORTED;
 }
 
--- chars.h
+++ chars.h
@@ -6,19 +6,13 @@ enum char_type {
 	CHAR_PRINTABLE,
 	CHAR_NEWLINE,
 	CHAR_BACKSPACE,
-	CHAR_ARROW_UP,
-	CHAR_ARROW_DOWN,
-	CHAR_ARROW_RIGHT,
-	CHAR_ARROW_LEFT,
+	CHAR_ESC,
 	CHAR_INTERRUPT,
 	CHAR_QUIT,
 	CHAR_UNSUPPORTED
 };
 
-enum char_type chars_get_esc_sequence(void);
-
 enum char_type chars_get_char_type(char ch);
-
 
 #endif
 
