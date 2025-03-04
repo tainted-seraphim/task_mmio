2c483455c8765d25c0be0af33868dd92b84c2853
tainted-seraphim <savosin_ya@mail.ru>
2025-03-04 17:22:55 +0300

    proper esc sequence input

--- esc.c
+++ esc.c
@@ -4,20 +4,11 @@
 
 enum esc_sequence esc_get_esc_sequence(void)
 {
-	int current_position = 1;
 	int check = 0;
-	char ch = 0;
 	char buffer[TASK_MMIO_ESC_MAX_LENGTH] = {27};
-	while (current_position < TASK_MMIO_ESC_MAX_LENGTH - 1) {
-		check = read(1, &ch, 1);
-		if (check == 0) {
-			break;
-		}
-		buffer[current_position] = ch;
-		current_position++;
-	}
-	buffer[current_position] = 0;
-	if (0 == strcmp(buffer, "\033")) {
+	/* This enables smooth arrow movement and history scrolling */
+	check = read(1, buffer + 1, TASK_MMIO_ESC_MAX_LENGTH - 1);
+	if (check == 0 || buffer[1] == 27) {
 		return ESC_KEY;
 	}
 	if (0 == strcmp(buffer, TASK_MMIO_ESC_CURSOR_UP)) {
