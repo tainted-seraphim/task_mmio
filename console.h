bb57eac5c14ebb997e88abfd034580042c797176
tainted-seraphim <savosin_ya@mail.ru>
2025-03-06 12:57:10 +0300

    preparation for char and esc sequence wrappers

--- console.h
+++ console.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include "command.h"
 #include "history.h"
+#include "chars.h"
 #include "esc.h"
 
 enum {
@@ -30,7 +31,7 @@ void console_do_action_for_esc(struct console *con, enum esc_sequence seq);
 
 void console_execute_command(struct console *con);
 
-typedef int (*console_command_handler)(struct console *con, size_t, char **);
+typedef int (*console_command_handler)(struct console *, size_t, char **);
 
 struct console_command_wrapper {
 	const char *command;
@@ -68,6 +69,52 @@ int console_command_mmwb(struct console *con, size_t argc, char **argv);
 int console_command_mmww(struct console *con, size_t argc, char **argv);
 
 int console_command_mmwd(struct console *con, size_t argc, char **argv);
+
+typedef int (*console_char_handler)(struct console *, char); 
+
+struct console_char_wrapper {
+	enum char_type type;
+	console_char_handler haldler;
+};
+
+int console_char_idle(struct console *con, char ch);
+
+int console_char_printable(struct console *con, char ch);
+
+int console_char_newline(struct console *con, char ch);
+
+int console_char_backspace(struct console *con, char ch);
+
+int console_char_esc(struct console *con, char ch);
+
+int console_char_interrupt(struct console *con, char ch);
+
+int console_char_quit(struct console *con, char ch);
+
+int console_char_unsupported(struct console *con, char ch);
+
+typedef int (*console_esc_handler)(struct console *, enum esc_sequence);
+
+struct console_esc_wrapper {
+	enum esc_sequence seq;
+	console_esc_handler handler;
+};
+
+int console_esc_esc_key(struct console *con, enum esc_sequence seq);
+
+int console_esc_arrow_up(struct console *con, enum esc_sequence seq);
+
+int console_esc_arrow_down(struct console *con, enum esc_sequence seq);
+
+int console_esc_arrow_right(struct console *con, enum esc_sequence seq);
+
+int console_esc_arrow_left(struct console *con, enum esc_sequence seq);
+
+int console_esc_home(struct console *con, enum esc_sequence seq);
+
+int console_esc_end(struct console *con, enum esc_sequence seq);
+
+int console_esc_unsupported(struct console *con, enum esc_sequence seq);
 
 #endif
 
